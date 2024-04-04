3fe74da13cc12c87f0daa4951bd29472104b9282
Akshat Narang <anarang@pageupsoft.com>
2024-04-04 18:52:12 +0530

    1st

---
+++ Exam/Enums/RoleType.cs
@@ -0,0 +1,9 @@
+﻿namespace Exam.Enums
+{
+    public enum RoleType
+    {
+        Admin = 0,
+        ExamAdmin = 1,
+        Employee = 2
+    }
+}
---
+++ Exam/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Exam.Enums;
+
+namespace Exam.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public RoleType roleType { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
